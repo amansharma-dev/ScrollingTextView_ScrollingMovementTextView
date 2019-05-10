467910767fae4977cd5117b2d7cc6e413da84226
Aman Sharma <amansharma.11@hotmail.com>
2019-05-10 15:37:21 +0530

    commit

--- app/src/main/java/com/example/scrollingtextview_scrollingmovementtextview/MainActivity.java
+++ app/src/main/java/com/example/scrollingtextview_scrollingmovementtextview/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.scrollingtextview_scrollingmovementtextview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textview);
+        textView.setMovementMethod(new ScrollingMovementMethod());
     }
 }
